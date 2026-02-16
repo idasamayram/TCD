dca01e3c437de0335345c2a697df5c770fb8a409
idasamayram <maryam.asadi.92@hotmail.com>
2026-02-16 19:11:38 +0100

    turned off torch.no_grad

--- scripts/run_analysis.py
+++ scripts/run_analysis.py
@@ -77,28 +77,27 @@ def run_crp_analysis(
     class_outputs = {class_id: [] for class_id in [0, 1]}
     class_sample_ids = {class_id: [] for class_id in [0, 1]}
     class_heatmaps = {class_id: [] for class_id in [0, 1]}
-    
+
     print(f"Running CRP analysis on {len(dataset)} samples...")
     sample_idx = 0
 
     # NOTE: Do NOT use torch.no_grad() here!
     # CRP/LRP requires gradient computation graph to propagate relevance.
     # model.eval() already handles dropout/batchnorm for inference.
-
     for data, labels in tqdm(dataloader, desc="Processing batches"):
         data = data.to(device)
         batch_size_actual = data.shape[0]
 
-        # Get model outputs
-        outputs = model(data)
+        # Get model outputs (for storing predictions)
+        with torch.no_grad():
+            outputs = model(data)
 
         # Prepare conditions for each sample
         conditions = [{"y": label.item()} for label in labels]
 
         # Compute CRP attribution with layer recording
         # requires_grad_(True) MUST be called OUTSIDE of no_grad context
-
-        data_with_grad = data.requires_grad_(True)
+        data_with_grad = data.clone().requires_grad_(True)
         heatmap, _, _, attr = attributor(
             data_with_grad,
             conditions,
@@ -113,7 +112,7 @@ def run_crp_analysis(
                 layer_rel = attr.relevances[layer]
                 # Use ChannelConcept to get per-filter relevance
                 concept_rel = cc.attribute(layer_rel, abs_norm=True)
-                eps_relevances[layer] = concept_rel.cpu()
+                eps_relevances[layer] = concept_rel.detach().cpu()
 
         # Store features grouped by class
         for i in range(batch_size_actual):
@@ -125,15 +124,16 @@ def run_crp_analysis(
                     class_features[class_id][layer].append(eps_relevances[layer][i])
 
             # Store outputs
-            class_outputs[class_id].append(outputs[i].cpu())
+            class_outputs[class_id].append(outputs[i].detach().cpu())
 
             # Store sample ID
             class_sample_ids[class_id].append(sample_idx + i)
 
             # Store heatmap
-            class_heatmaps[class_id].append(heatmap[i].cpu().numpy())
+            class_heatmaps[class_id].append(heatmap[i].detach().cpu().numpy())
 
         sample_idx += batch_size_actual
+
 
 
     # Save results per class
